7bb3bd2d7342a7fd514b0e00ce9353a3db5b14b7
Insidium <ryanchallen@outlook.com>
2020-03-05 11:53:57 +1000

    Added routes and shells of views for new items, individual items and artists and a list of artists

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  #Home Page
   root to: "users#index"
+  get "/", to: "users#index", as: "home"
+
+  #Create new artwork item
+  get "/items/new", to: "items#new", as: "new_art"
+  post "/items", to: "items#create"
+
+  #Show individual piece of art
+  get "/items/:id", to: "items#show", as: "item"
+
+  #Show Artists
+  get "/artists", to: "artists#index", as: "artists"
+
+  # Show individual Author
+  get "/artists/:id", to: "artists#show", as: "artist"
+
 end
