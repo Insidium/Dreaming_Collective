db0b1e9a8e07376e527d77da9624a6392f387986
Insidium <ryanchallen@outlook.com>
2020-03-12 15:08:54 +1000

    Added comments on components missing them, added redirect to dashboard on successful login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,9 @@ class ApplicationController < ActionController::Base
         devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address])
         devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address])
       end
+
+      # Redirect to dashboard on login success
+      def after_sign_in_path_for(resource)
+        dashboard_path(current_user.id)
+      end
 end
